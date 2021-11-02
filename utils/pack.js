23d6ee08b1c1cdbf1c3be4f74dc7d9900a317d75
Gert Mertes <hey@gertmertes.be>
2021-11-02 15:24:09 +0800

    Refactor pack.js

--- utils/pack.js
+++ utils/pack.js
@@ -1,13 +1,10 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
-
+const PACKAGE = require('../package.json');
 const fse = require('fs-extra');
 
-
 // =============================================================================
-
-
 const kScriptDir = path.dirname(require.main.filename);
 const kProjectDir = path.join(kScriptDir, '..');
 
@@ -15,19 +12,18 @@ const kBuildDir = path.join(kProjectDir, 'build');
 const kDistDir = path.join(kProjectDir, 'dist');
 const kModulesDir = path.join(kProjectDir, 'node_modules');
 
-const kPackageName = process.env.npm_package_name;
-const kPackageVersion = process.env.npm_package_version;
-
-
+const kPackageName = PACKAGE.name;
+const kPackageVersion = PACKAGE.version;
 // -----------------------------------------------------------------------------
-
 
 const browsers = ['chrome', 'firefox'];
 browsers.forEach(browser => {
   const buildDir = path.join(kBuildDir, browser);
   const outName = `${kPackageName}_v${kPackageVersion}_${browser}.zip`;
+  const srcPath = path.join(buildDir);
+  const outPath = path.join(kDistDir, outName);
 
-  if (!fs.lstatSync(buildDir).isDirectory()) {
+  if (!fs.existsSync(buildDir)) {
     console.error(`Error: build folder "${buildDir}" unavailable`);
     process.exit(1);
   }
@@ -36,14 +32,10 @@ browsers.forEach(browser => {
   fse.ensureDirSync(kDistDir);
 
   console.log(`Archiving for "${browser}" ...`);
-  const archive = (() => {
-    const srcPath = path.join(buildDir);
-    const outPath = path.join(kDistDir, outName);
 
-    fse.removeSync(outPath);
-    child_process.spawnSync('zip', ['-j', '-r', '-9', outPath, srcPath],
-      { stdio: 'inherit' });
-  })();
+  fse.removeSync(outPath);
+  child_process.spawnSync('zip', ['-j', '-r', '-9', outPath, srcPath],
+    { stdio: 'inherit' });
 
   console.log(`Packed for "${browser}" !!`);
 });
